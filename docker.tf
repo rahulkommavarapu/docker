ae42cc69a7c056f2842eb2e7c1895d4737e35e4c
Rahul kommavarapu <Rahulkommavarapu225@gmail.com>
2026-04-11 14:31:02 +0530

    test

--- docker.tf
+++ docker.tf
@@ -20,9 +20,9 @@ resource "aws_security_group" "allow_all_docker" {
   description = "Allow TLS inbound traffic and all outbound traffic"
 
   ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
